ba088d11161384bcac496b552d67cf51e7e9a79c
bMil21 <bmil_21@yahoo.com>
2021-07-19 16:10:15 -0400

    add mat input

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatListModule],
+  imports: [CommonModule, MatButtonModule, MatListModule, MatInputModule],
   declarations: [],
-  exports: [MatButtonModule, MatListModule]
+  exports: [MatButtonModule, MatListModule, MatInputModule]
 })
 export class AppMaterialModule {}
